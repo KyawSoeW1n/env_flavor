85429973720085fbcf4e0f9cf1c8df92e1563f39
ksw321 <kyawsoewin.fyimyanmar@gmail.com>
2024-09-11 02:14:59 +0700

    Add ENV with ICON

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,57 +1,15 @@
-import 'package:env_flavor/repo/http_repo.dart';
+import 'package:env_flavor/pages/sample_section.dart';
 import 'package:flutter/material.dart';
-
 import '../flavors.dart';
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final httpRepo = HttpRepoImpl();
-  String data = "Empty";
-
-  @override
-  void initState() {
-    fetchData();
-    super.initState();
-  }
-
-  void fetchData() {
-    httpRepo.fetchData().then((value) {
-      setState(() {
-        data = value;
-      });
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(F.title),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              'Hello ${F.title}',
-            ),
-            ElevatedButton(
-              onPressed: () {
-                fetchData();
-              },
-              child: Text("Refresh"),
-            ),
-            Expanded(
-              child: Text(
-                data,
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: SampleSection()
     );
   }
 }
